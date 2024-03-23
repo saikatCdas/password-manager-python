db8860c1a4ebd4eccbd200bdc9f1b5c9e3fc8cc3
Saikat <saikatdas45857@gmail.com>
2024-03-23 20:05:21 +0600

    search added

--- main.py
+++ main.py
@@ -43,14 +43,43 @@ def save():
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
             data.update(new_data)
-        with open("data.json", "w") as data_file:
-            json.dump(data, data_file, indent=4)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+
+def find_password():
+    website = website_entry.get()
+    if len(website) == 0:
+        messagebox.showinfo(title=website, message="No input")
+        return
+    try:
+        with open("data.json", "r") as data_file:
+            # Reading old data
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message="File not found")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -72,8 +101,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=38)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -86,5 +115,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
